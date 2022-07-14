2a921cd0bb5d05a5dc48d542d57dec3c9bf7aa9d
Bobby Lee <bobby@bobbylee.dev>
2022-07-13 21:59:36 -0700

    fix: button name

--- wp-content/themes/sapphire/blocks/sapphire-video-popup/sapphire-video-popup.php
+++ wp-content/themes/sapphire/blocks/sapphire-video-popup/sapphire-video-popup.php
@@ -7,7 +7,7 @@ if (array_key_exists('buttonText', $attributes) && array_key_exists('video', $at
 
 	<button class="button secondary yt-button"><?= $attributes['buttonText'] ?></button>
 	<div class="yt-overlay">
-		<button class="close-popup"><i></i></button>
+		<button class="close-popup" aria-label="close popup"><i></i></button>
 		<div class="yt-wrapper">
 			<div class="ytvideo" data-video="<?= $attributes['video'] ?>"></div>
 		</div>
